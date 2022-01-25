8e8308db23f0003c47fe4338db802df3f6c14436
esra balci <esrambalci@hotmail.com>
2022-01-24 22:13:01 -0500

    first commit

--- src/Practice1/Esra.java
+++ src/Practice1/Esra.java
@@ -1,4 +1,9 @@
 package Practice1;
 
 public class Esra {
+
+    public static void main(String[] args) {
+        System.out.println("initial commit in local");
+        System.out.println("my first coding");
+    }
 }
