e7bb1a3ee4d9bcf1dcdd9a4a4a06462bb2a2a8bf
esra balci <esrambalci@hotmail.com>
2022-01-24 22:23:01 -0500

    for push

--- src/Practice1/Esra.java
+++ src/Practice1/Esra.java
@@ -5,5 +5,6 @@ public class Esra {
     public static void main(String[] args) {
         System.out.println("initial commit in local");
         System.out.println("my first coding");
+        //for push
     }
 }
